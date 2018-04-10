d1629496794ba11aea400d439d9268772b6bbe29
cristianmmuresan <cristian@wearewip.com>
2018-04-10 23:06:24 +0300

    added random scrolling activity

--- app/src/main/java/com/example/android/popularmovies/main/MoviesAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/main/MoviesAdapter.java
@@ -13,6 +13,7 @@ import com.example.android.networkmodule.model.Movie;
 import com.example.android.popularmovies.ConstantMoviePosterSizes;
 import com.example.android.popularmovies.R;
 import com.example.android.popularmovies.details.MovieDetailsActivity;
+import com.example.android.popularmovies.details.ScrollingActivity;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -78,7 +79,7 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
     }
 
     private void openMovieDetails(Movie movie) {
-        Intent intent = new Intent(context, MovieDetailsActivity.class);
+        Intent intent = new Intent(context, ScrollingActivity.class);
         intent.putExtra(MovieDetailsActivity.EXTRA_MOVIE, movie);
         context.startActivity(intent);
     }
